b95606a91e295ed5601af4fd87323a070d4da13c
vicvillarroel <165968225+Kiki0w0@users.noreply.github.com>
2024-10-03 18:10:12 -0300

    Create ArregloRedimensionableDeRecordatorios.java

---
+++ ArregloRedimensionableDeRecordatorios.java
@@ -0,0 +1,58 @@
+package aed;
+
+class ArregloRedimensionableDeRecordatorios {
+    
+    private int longitud;
+    private Recordatorio[] arreglo;
+
+    public ArregloRedimensionableDeRecordatorios() {
+        // Implementar
+        this.longitud = 0;
+        this.arreglo = new Recordatorio[0];
+    
+    }
+
+    public int longitud() {
+        // Implementar
+        return longitud;
+    }
+
+    public void agregarAtras(Recordatorio i) {
+        // Implementar
+        longitud = longitud() + 1;
+
+        Recordatorio[] arreglo_nuevo = new Recordatorio[longitud];
+
+        for (int e = 0; e < longitud - 1; e++) {
+            
+            arreglo_nuevo[e] = arreglo[e];
+
+            arreglo = arreglo_nuevo;
+        }
+        arreglo_nuevo[longitud - 1] = i;
+        arreglo = arreglo_nuevo;
+
+    }
+
+    public Recordatorio obtener(int i) {
+        // Implementar
+        return arreglo[i];
+    }
+
+    public void quitarAtras() {
+        // Implementar
+    }
+
+    public void modificarPosicion(int indice, Recordatorio valor) {
+        // Implementar
+    }
+
+    public ArregloRedimensionableDeRecordatorios(ArregloRedimensionableDeRecordatorios vector) {
+        // Implementar
+    }
+
+    public ArregloRedimensionableDeRecordatorios copiar() {
+        // Implementar
+        return null;
+    }
+}
